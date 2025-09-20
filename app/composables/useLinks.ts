331d26cabe1a822c327fc812566054bac7e70bb3
Alberto Alejandro <alberto196g@gmail.com>
2025-09-20 19:50:37 -0400

    feat: update portfolio content and structure, including personal information and links

--- app/composables/useLinks.ts
+++ app/composables/useLinks.ts
@@ -3,27 +3,33 @@ import type { NavigationMenuItem } from '@nuxt/ui'
 export function useLinks() {
   const localePath = useLocalePath()
 
-  const links: ComputedRef<NavigationMenuItem[]> = computed(() => [{
+  const links: ComputedRef<NavigationMenuItem[]> = computed(() => [
+  {
     label: 'Home',
     icon: 'i-lucide-home',
     to: localePath('/')
-  }, {
+  },
+  {
     label: 'Projects',
     icon: 'i-lucide-folder',
     to: localePath('/projects')
-  }, {
+  },
+  {
     label: 'Blog',
     icon: 'i-lucide-file-text',
     to: localePath('/blog')
-  }, {
-    label: 'Speaking',
-    icon: 'i-lucide-mic',
-    to: localePath('/speaking')
-  }, {
+  },
+  // {
+    // label: 'Speaking',
+    // icon: 'i-lucide-mic',
+    // to: localePath('/speaking')
+  // },
+  {
     label: 'About',
     icon: 'i-lucide-user',
     to: localePath('/about')
-  }])
+  }
+])
 
   return links
 }
