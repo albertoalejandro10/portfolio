332a6d040b105010a6a4034cf35448cdbbd9d03e
Alberto Alejandro <alberto196g@gmail.com>
2025-10-07 00:07:05 -0400

    feat: wrap blog/project posts in ClientOnly for improved SSR handling

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -68,30 +68,32 @@ useSeoMeta({
       }"
     >
       <UBlogPosts orientation="vertical">
-        <Motion
-          v-for="(post, index) in posts"
-          :key="index"
-          :initial="{ opacity: 0, transform: 'translateY(10px)' }"
-          :while-in-view="{ opacity: 1, transform: 'translateY(0)' }"
-          :transition="{ delay: 0.2 * index }"
-          :in-view-options="{ once: true }"
-        >
-          <UBlogPost
-            variant="naked"
-            orientation="horizontal"
-            :to="post.path"
-            v-bind="post"
-            :ui="{
-              root: 'md:grid md:grid-cols-2 group overflow-visible transition-all duration-300',
-              image:
-                'group-hover/blog-post:scale-105 rounded-lg shadow-lg border-4 border-muted ring-2 ring-default',
-              header:
-                index % 2 === 0
-                  ? 'sm:-rotate-1 overflow-visible'
-                  : 'sm:rotate-1 overflow-visible'
-            }"
-          />
-        </Motion>
+        <ClientOnly>
+          <Motion
+            v-for="(post, index) in posts"
+            :key="index"
+            :initial="{ opacity: 0, transform: 'translateY(10px)' }"
+            :while-in-view="{ opacity: 1, transform: 'translateY(0)' }"
+            :transition="{ delay: 0.2 * index }"
+            :in-view-options="{ once: true }"
+          >
+            <UBlogPost
+              variant="naked"
+              orientation="horizontal"
+              :to="post.path"
+              v-bind="post"
+              :ui="{
+                root: 'md:grid md:grid-cols-2 group overflow-visible transition-all duration-300',
+                image:
+                  'group-hover/blog-post:scale-105 rounded-lg shadow-lg border-4 border-muted ring-2 ring-default',
+                header:
+                  index % 2 === 0
+                    ? 'sm:-rotate-1 overflow-visible'
+                    : 'sm:rotate-1 overflow-visible'
+              }"
+            />
+          </Motion>
+        </ClientOnly>
       </UBlogPosts>
     </UPageSection>
   </UPage>
